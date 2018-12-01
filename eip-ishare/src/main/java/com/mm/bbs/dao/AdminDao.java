e881a72296624cd8cdd40b0a0fca5f622e0290f2
davis <davis@DESKTOP-KK1O8MA>
2018-12-01 10:52:39 +0800

    DAO Inf

--- eip-ishare/src/main/java/com/mm/bbs/dao/AdminDao.java
+++ eip-ishare/src/main/java/com/mm/bbs/dao/AdminDao.java
@@ -1,15 +1,18 @@
 package com.mm.bbs.dao;
 
-
+import com.mm.bbs.pojo.Admin;
 import java.util.List;
 
-import com.mm.bbs.pojo.Admin;
-import com.mm.bbs.pojo.DoorSensorDtl;
 
-public interface AdminDao extends BaseDao<Admin, String>{
-	
-	//登录 管理员
-	public List<Admin> findByAdmin(String name);
-	
-//	public List<Admin> findAdmins();
+public abstract interface AdminDao
+  extends BaseDao<Admin, String>
+{
+  public abstract List<Admin> findByAdmin(String paramString);
+  /**
+   * paganation
+   * @param paramInt1
+   * @param paramInt2
+   * @return
+   */
+  public abstract List<Admin> find(int paramInt1, int paramInt2);
 }
--- eip-ishare/src/main/java/com/mm/bbs/dao/DoorSensorDtlDao.java
+++ eip-ishare/src/main/java/com/mm/bbs/dao/DoorSensorDtlDao.java
@@ -1,17 +1,63 @@
 package com.mm.bbs.dao;
-import java.io.Serializable;
-import java.util.Date;
-import java.util.List;
 
-import com.mm.bbs.inf.GenericInf;
+import java.util.List;
 import com.mm.bbs.pojo.DoorSensorDtl;
+import com.mm.bbs.util.CheckState;
  
  
 
  
 public interface DoorSensorDtlDao extends BaseDao<DoorSensorDtl, String> {
-//	public DoorSensor findDiviceById(String deviceId);
-	public List<DoorSensorDtl> findDiviceByStaffCheckOnDate(boolean staffCheckInd,Date fdate);
-	public List<DoorSensorDtl> findDiviceByStaffCheck(boolean staffCheckInd,Date fdate,Date todate);
-	 
+//	public DoorSensor findDeviceById(String deviceId);
+	/**
+	 * 
+	 * @param i=0,返回当天所有设备数据
+	 * @return
+	 */
+	public List<DoorSensorDtl> findDeviceByInd(Integer i);
+	public List<DoorSensorDtl> findDeviceByChnlNo(Integer i,Integer channelNo);
+//	public List<DoorSensorDtl> findDeviceByStaffCheckOnDate(CheckState checkState,String fdate);
+	public List<DoorSensorDtl> findDeviceBtwDatetime(CheckState checkState,String fdate,String todate);
+
+//	public List<DoorSensorDtl> findAutoCheckDeviceInTime(String channelNo,String fdate);
+	/**
+	 * 查询某时刻的记录
+	 * @param inputDt 数据记录时间
+	 * @param staffCheckInd 自动巡查 为 F, 人工巡查为T
+	 * @param channelNo 通道号 1 2 3 4
+	 * @param doorState 门关闭状态 Close Open
+	 * @return
+	 */
+	public List<DoorSensorDtl> findDeviceByParams(String inputDt,String staffCheckInd,Integer channelNo,String doorState);
+	/**
+	 * 查询当天或昨天的记录
+	 * @param i=0 查询当天记录,i=1 查询昨天记录
+	 * @param staffCheckInd 自动巡查 为 F, 人工巡查为T
+	 * @param channelNo 通道号 1 2 3 4
+	 * @param doorState 门关闭状态 Close Open
+	 * @return
+	 */
+	public List<DoorSensorDtl> findDeviceByParams(Integer i,String staffCheckInd,Integer channelNo,String doorState);
+	/**
+	 * 查询当天或昨天的记录
+	 * @param i=0 查询当天记录,i=1 查询昨天记录
+	 * @param deviceId 设备号
+	 * @param staffCheckInd 自动巡查 为 F, 人工巡查为T
+	 * @param channelNo 通道号 1 2 3 4
+	 * @param doorState 门关闭状态 Close Open
+	 * @return
+	 */
+	public List<DoorSensorDtl> findDeviceByParams(Integer i,String deviceId,String staffCheckInd,Integer channelNo,String doorStatus);
+	/**
+	 * 自动查询当天 每小时 的所有记录
+	 * @param staffCheckInd
+	 * @param channelNo
+	 * @param doorState
+	 * @return
+	 */
+	public List<DoorSensorDtl> findDeviceByParamsOnHour(String staffCheckInd,Integer channelNo,String doorState);
+//	public Long getDoorSensorCountAll();
+//	public Long getDoorSensorCountByState(String ind,String inputDt);
+//	public Long getDoorSensorCountByState(String ind,Date inputDt);
+//	public List<DoorSensorDtl> findManulCheckDevice(String todate);
 }
