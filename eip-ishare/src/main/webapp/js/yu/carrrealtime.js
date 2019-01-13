8644a2aaca3233a0ef7c22684482b92b647422db
davis <davis@DESKTOP-KK1O8MA>
2019-01-13 14:45:01 +0800

    car page

--- eip-ishare/src/main/webapp/js/yu/carrrealtime.js
+++ eip-ishare/src/main/webapp/js/yu/carrrealtime.js
@@ -1,6 +1,7 @@
 $(function(){
 	//car sensor real time 
-	var ws2 = new WebSocket("ws://18.191.197.106:8080/eip-ishare/carsensordtlws.do");
+	//var ws2 = new WebSocket("ws://18.191.197.106:8080/eip-ishare/carsensordtlws.do");
+	var ws2 = new WebSocket("ws://127.0.0.1:8080/eip-ishare/carsensordtlws.do");
 
 	ws2.onopen = function()
 
@@ -14,11 +15,17 @@ $(function(){
 	{
 	  console.log(evt.data)
 	  var data=JSON.parse(evt.data);
-	  var time=data.inputDt.substring(data.inputDt.length-8)
-	  data=data.loss;
-	  for(var i=0;i<data.length;i++){
-	  	  var list="<tr class='gradeX'><td>"+time+"</td><td>"+data[i].deviceId+"</td><td>"+data[i].carStatus+"</td><td>"+data[i].nbSignalPwr+"</td><td>"+data[i].battVol+"</td><td>"+data[i].dataType+"</td><td>"+data[i].dataLen+"</td></tr>"
-		  $(".ly-car tbody").append(list);
+	  var time=timestampToTime(data.inputDt);
+		 // data.inputDt.substring(data.inputDt.length-8)
+	  //data=data.loss;
+	  if(data !=undefined){
+	  //for(var i=0;i<data.length;i++){
+	  	 // var list="<tr class='gradeX'><td>"+time+"</td><td>"+data[i].deviceId+"</td><td>"+data[i].carStatus+"</td><td>"+data[i].nbSignalPwr+"</td><td>"+data[i].battVol+"</td><td>"+data[i].dataType+"</td><td>"+data[i].dataLen+"</td></tr>"
+	  	  var list="<tr class='gradeX'><td>"+time+"</td><td>"+data.device.deviceDesc+"</td><td>"+data.carStatus+"</td><td>"+data.nbSignalPwr+"</td><td>"+data.battVol+"</td><td>"+data.dataType+"</td><td>"+data.dataLen+"</td></tr>"
+		 
+	  	  $(".ly-car tbody").append(list);
+	  	  
+	 // }
 	  }
 	 }
 
@@ -37,4 +44,16 @@ $(function(){
 	  console.log("WebSocketError!");
 
 	};
+	
+	function timestampToTime(timestamp) {
+		   var date = new Date(timestamp);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
+		   Y = date.getFullYear() + '-';
+		   M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+		   D = date.getDate() + ' ';
+		   h = date.getHours() + ':';
+		   m = date.getMinutes() + ':';
+		   s = date.getSeconds();
+		   return Y+M+D;
+	}
+
 })
